d98128037b2a86ba01795aa679c7d189a667eac7
Benjamin Peters <benjamin.peters@gmail.com>
2018-04-02 16:25:35 -0400

    changed port

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-const port = 3000;
-
-
 // ----------------------------------------
 // | MIDDLEWARE                           |
 // ----------------------------------------
