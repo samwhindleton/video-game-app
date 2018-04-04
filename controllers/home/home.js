97572bc09c8e344db2875a999f2779179245acd3
Benjamin Peters <benjamin.peters@gmail.com>
2018-04-04 00:56:33 -0400

    added sessions

--- controllers/home/home.js
+++ controllers/home/home.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Game = require('../../models/game.js');
 
 router.post('/', (req, res) => {
+  req.body.user_id = req.session.currentUser._id
   Game.create(req.body, (err, newGame) => {
     res.json(newGame)
   });
