b107c158b6efefa39242c7e3e4a7525b0983eae8
Benjamin Peters <benjamin.peters@gmail.com>
2018-04-04 13:17:56 -0400

    comments

--- controllers/home/home.js
+++ controllers/home/home.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Game = require('../../models/game.js');
 
+//Create new game with user id
 router.post('/', (req, res) => {
   req.body.user_id = req.session.currentuser._id
   Game.create(req.body, (err, newGame) => {
--- controllers/users/login.js
+++ controllers/users/login.js
@@ -4,25 +4,27 @@ const bcrypt = require('bcrypt');
 
 const User = require('../../models/users.js');
 
+//Login
 router.post('/', (req, res) => {
   User.findOne({
     username: req.body.username
   }, (err, user) => {
     if( bcrypt.compareSync(req.body.password, user.password) ){
-        req.session.currentuser = user;
-        res.status(201).json({
-            status:201,
-            message:'session created'
-        });
+      req.session.currentuser = user;
+      res.status(201).json({
+          status:201,
+          message:'session created'
+      });
     } else {
-        res.status(401).json({
-            status:401,
-            message:'login failed'
-        });
+      res.status(401).json({
+          status:401,
+          message:'login failed'
+      });
     }
   })
 });
 
+//Logout
 router.delete('/', (req, res) => {
     req.session.destroy(() => {
         res.status(200).json({
